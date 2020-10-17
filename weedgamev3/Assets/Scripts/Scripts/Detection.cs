8a30e38951a8a38ec377a5869ebeb009ff1f3ebf
DanZambe <67613470+DanZambe@users.noreply.github.com>
2020-10-17 14:11:03 +1100

    Grid, Triggers
    Grin n Trigger

---
+++ weedgamev3/Assets/Scripts/Scripts/Detection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        print("Run GridUpdate");
+    }
+}
